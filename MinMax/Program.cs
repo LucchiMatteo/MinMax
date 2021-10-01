1d16500cb819e06fc16e4a3c4f1e3448065b5d0d
LucchiMatteo <mlucchi79@gmail.com>
2021-10-01 09:31:54 +0200

    V1.1: aggiunto ciclo per ricerca minimo

--- MinMax/Program.cs
+++ MinMax/Program.cs
@@ -6,7 +6,17 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vettore = new int[5] { 3, 6, -3, 5, 7 };
+
+            int min = vettore[0];
+
+            for(int i=1; i<5; i++)
+            {
+                if (vettore[i] < min)
+                    min = vettore[i];
+            }
+
+            Console.WriteLine(min);
         }
     }
 }
